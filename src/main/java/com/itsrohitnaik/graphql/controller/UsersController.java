196c66d2573c41f073e8f3d3f3ab9e90c74dfa2f
Rohit Naik <rohit24naik@gmail.com>
2020-11-27 00:30:50 +0530

    changed endpoint

--- src/main/java/com/itsrohitnaik/graphql/controller/UsersController.java
+++ src/main/java/com/itsrohitnaik/graphql/controller/UsersController.java
@@ -1,23 +1,16 @@
 package com.itsrohitnaik.graphql.controller;
 
-import com.itsrohitnaik.graphql.model.Users;
 import com.itsrohitnaik.graphql.services.GraphqlService;
-import com.itsrohitnaik.graphql.services.UserServices;
 import graphql.ExecutionResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RestController
 public class UsersController implements UsersControllerInterface {
-
-    @Autowired
-    UserServices userServices;
 
     @Autowired
     GraphqlService graphqlService;
